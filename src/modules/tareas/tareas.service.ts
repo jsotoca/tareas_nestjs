ca28148c0a4be6f0589a818085478afb99e46177
jsotoca <jsotoca@unprg.edu.pe>
2020-05-28 13:33:02 -0500

    Se configuro CRUD tareas en service

--- src/modules/tareas/tareas.service.ts
+++ src/modules/tareas/tareas.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import TareaRepository from './tareas.repository';
+import CrearTareaDTO from './dto/crear-tarea.dto';
+import FiltrarTareasDTO from './dto/filtrar-tareas.dto';
+import { EstadoTarea } from './tareas.estados';
 
 @Injectable()
-export class TareasService {}
+export class TareasService {
+    constructor(
+        @InjectRepository(TareaRepository)
+        private tareaRepository:TareaRepository
+    ){}
+
+    async obtenerTareas(filtrarTareasDTO:FiltrarTareasDTO){
+        return await this.tareaRepository.obtenerTareas(filtrarTareasDTO);
+    }
+
+    async obtenerTareaPorID(id:number){
+        const encontrado = await this.tareaRepository.findOne(id);
+        if(!encontrado) throw new NotFoundException(`No se encontro tarea con id ${id}`);
+        return encontrado;
+    }
+
+    async crearTarea(crearTareaDTO:CrearTareaDTO){
+        return await this.tareaRepository.crearTarea(crearTareaDTO);
+    }
+
+    async actualizarTarea(id:number,estado:EstadoTarea){
+        const actualizado = await this.obtenerTareaPorID(id);
+        actualizado.estado = estado;
+        await actualizado.save();
+        return actualizado;
+    }
+
+    async eliminarTarea(id:number){
+        const eliminado = await this.tareaRepository.delete(id);
+        if(eliminado.affected !== 0) throw new NotFoundException(`No se encontro tarea con id ${id}`);
+        return `Se elimino la tarea con id ${id}`;
+    }
+}
