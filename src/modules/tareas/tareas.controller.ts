0955788a97ae061d5863c658703d903336e5fbc3
jsotoca <jsotoca@unprg.edu.pe>
2020-05-28 13:39:23 -0500

    Se configuro CRUD tareas en controller

--- src/modules/tareas/tareas.controller.ts
+++ src/modules/tareas/tareas.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import { TareasService } from './tareas.service';
+import { Controller, Get, Body, ValidationPipe, Param, ParseIntPipe, Post, Put, Delete } from '@nestjs/common';
+import FiltrarTareasDTO from './dto/filtrar-tareas.dto';
+import CrearTareaDTO from './dto/crear-tarea.dto';
+import { EstadoTarea } from './tareas.estados';
 
 @Controller('tareas')
-export class TareasController {}
+export class TareasController {
+    constructor(
+        private tareasService:TareasService
+    ){}
+
+    @Get()
+    async obtenerTareas(@Body(ValidationPipe) filtrarTareasDTO:FiltrarTareasDTO){
+        return await this.tareasService.obtenerTareas(filtrarTareasDTO);
+    }
+
+    @Get('/:id')
+    async obtenerTarea(
+        @Param('id',ParseIntPipe) id:number
+    ){
+        return await this.tareasService.obtenerTareaPorID(id);
+    }
+
+    @Post()
+    async crearTarea(
+        @Body(ValidationPipe) crearTareaDTO:CrearTareaDTO
+    ){
+        return await this.tareasService.crearTarea(crearTareaDTO);
+    }
+
+    @Put('/:id')
+    async actualizarTarea(
+        @Param('id',ParseIntPipe) id:number,
+        @Body('estado') estado:EstadoTarea
+    ){
+        return await this.tareasService.actualizarTarea(id,estado);
+    }
+
+    @Delete('/:id')
+    async eliminarTarea(
+        @Param('id',ParseIntPipe) id:number
+    ){
+        return await this.tareasService.eliminarTarea(id);
+    }
+}
